6c5569491cb58774548fd6e4267a167407d9f194
Akash Kumar <akash211@gmail.com>
2022-05-29 16:04:21 +0000

    dependencies

--- dags/user_processing.py
+++ dags/user_processing.py
@@ -3,6 +3,7 @@ from airflow.providers.sqlite.operators.sqlite import SqliteOperator
 from airflow.providers.http.sensors.http import HttpSensor
 from airflow.providers.http.operators.http import SimpleHttpOperator
 from airflow.operators.python import PythonOperator
+from airflow.operators.bash import BashOperator
 from datetime import datetime
 import json
 from pandas import json_normalize
@@ -11,19 +12,22 @@ default_args = {"start_date": datetime(2020, 1, 1)}
 
 
 def _processing_user(ti):
-    users = ti.xcom_pull(task_id=['extracting_user'])
-    if not len(users) or 'results' not in users[0]:
-        raise ValueError('User is empty')
-    user = users[0]['results'][0]
-    processed_user = json_normalize({
-        'firstname': user['name']['first'],
-        'lastname': user['name']['last'],
-        'country': user['location']['country'],
-        'username': user['login']['username'],
-        'password': user['login']['password'],
-        'email': user['email']
-    })
-    processed_user.to_csv('/tmp/processed_user.csv', index=None, header=False)
+    users = ti.xcom_pull(task_ids=["extracting_user"])
+    if not len(users) or "results" not in users[0]:
+        raise ValueError("User is empty")
+    user = users[0]["results"][0]
+    processed_user = json_normalize(
+        {
+            "firstname": user["name"]["first"],
+            "lastname": user["name"]["last"],
+            "country": user["location"]["country"],
+            "username": user["login"]["username"],
+            "password": user["login"]["password"],
+            "email": user["email"],
+        }
+    )
+    processed_user.to_csv("/tmp/processed_user.csv", index=None, header=False)
+
 
 with DAG(
     "user_processing",
@@ -60,4 +64,13 @@ with DAG(
         log_response=True,
     )
 
-    processing_user = PythonOperator(task_id='processing_user', python_callable=_processing_user)
+    processing_user = PythonOperator(
+        task_id="processing_user", python_callable=_processing_user
+    )
+
+    storing_user = BashOperator(
+        task_id="storing_user",
+        bash_command='echo -e ".separator ","\n.import /tmp/processed_user.csv users" | sqlite3 /home/airflow/airflow/airflow.db',
+    )
+
+    creating_table >> is_api_available >> extracting_user >> processing_user >> storing_user
